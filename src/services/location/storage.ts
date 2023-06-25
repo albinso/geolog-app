095b7720fbe26f729cb60ab4ffb51df671c50e54
Albin Söderholm <albin.johso@gmail.com>
2023-06-25 19:12:39 +0200

    Minimum distance check

--- src/services/location/storage.ts
+++ src/services/location/storage.ts
@@ -24,6 +24,37 @@ export async function setLocations(locations: LocationObject[]): Promise<void> {
   await AsyncStorage.setItem(locationStorageName, JSON.stringify(locations));
 }
 
+export function getDistanceFromLatLonInM(lat1,lon1,lat2,lon2) {
+  const R = 6371; // Radius of the earth in km
+  const dLat = deg2rad(lat2-lat1);  // deg2rad below
+  const dLon = deg2rad(lon2-lon1); 
+  const a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2); 
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  const d = R * c; // Distance in km
+  return d*1000.0;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
+}
+
+export const shouldUse = (location, lastLocationPosted) => {
+  let x1 = parseFloat(location.latitude);
+  let y1 = parseFloat(location.longitude);
+  let x2 = parseFloat(lastLocationPosted.latitude);
+  let y2 = parseFloat(lastLocationPosted.longitude);
+
+
+  if (getDistanceFromLatLonInM(x1, y1, x2, y2) < 10) { // should be equivalent to ~10m
+    return false;
+  }
+  return true;
+
+}
+
 /**
  * Add a new location to the storage.
  * This is a helper to append a new location to the storage.
@@ -31,8 +62,17 @@ export async function setLocations(locations: LocationObject[]): Promise<void> {
 export async function addLocation(location: LocationObject): Promise<LocationObject[]> {
   const existing = await getLocations();
   const locations = [...existing, location];
+  const lastLocationPosted = locations[0];
+  if (!shouldUse(location, existing[-1])) {
+    console.log("[storage] skipped location due to distance to previous point");
+    return;
+  }
+  
+
   await setLocations(locations);
   console.log('[storage]', 'added location -', locations.length, 'stored locations');
+  
+
   if (locations.length >= 20 || locations.length >= 1 && location.timestamp - locations[0].timestamp >= 60 * 1000 * 5) {
     postLocation(locations).then(() => {
       clearLocations();
@@ -40,6 +80,8 @@ export async function addLocation(location: LocationObject): Promise<LocationObj
   }
   return locations;
 }
+
+
 
 /**
  * Reset all stored locations.
