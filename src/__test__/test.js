c6e5676f64cf4916f9aca8ff64cfc6c12b52dcfc
Albin Söderholm <albin.johso@gmail.com>
2023-06-26 23:05:25 +0200

    Nightmode

--- src/__test__/test.js
+++ src/__test__/test.js
@@ -11,19 +11,27 @@ describe("shouldUse", () => {
             latitude: 0.0001,
             longitude: 0
         };
-        expect(shouldUse(location, lastLocationPosted)).toBe(true);
+        expect(shouldUse(location, lastLocationPosted)[0]).toBe(true);
     });
 
     it("should return false if distance is less than 10m", () => {
         let location = {
-            latitude: 0,
-            longitude: 0
+            coords: {
+                latitude: 0,
+                longitude: 0
+            },
+            timestamp: 1300000000000
         };
         let lastLocationPosted = {
-            latitude: 0.00001,
-            longitude: 0
+            coords: {
+                latitude: 0.00001,
+                longitude: 0
+            },
+            timestamp: 1300000000005
         };
-        expect(shouldUse(location, lastLocationPosted)).toBe(false);
+        const [pred, cause] = shouldUse(location, lastLocationPosted);
+        console.log("Cause: " + cause);
+        expect(pred).toBe(false);
     });
 
     it("should calc distance to be less than 10m", () => {
